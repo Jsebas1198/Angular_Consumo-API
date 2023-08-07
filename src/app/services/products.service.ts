53b9dc16d3d9e9d47bb439b26f4d7223fe43b527
Juan Sebastian Fernandez <juan.fernandez@itti.digital>
2023-08-07 15:13:21 -0600

    Implementación de paginación

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import {
   CreateProductDTO,
@@ -14,10 +15,27 @@ export class ProductsService {
   private apiUrl = 'https://api.escuelajs.co/api/v1/products';
   constructor(private http: HttpClient) {}
 
-  //Trae todos los productos
-  getAllProducts() {
-    return this.http.get<Product[]>(this.apiUrl);
+  //Trae todos los productos, se le agregó paginación
+  // getAllProducts() {
+  //   return this.http.get<Product[]>(this.apiUrl);
+  // }
+
+  getAllProducts(limit?: number, offset?: number) {
+    let params = new HttpParams();
+    if (limit != undefined && offset != undefined) {
+      params = params.set('limit', limit);
+      params = params.set('offset', offset);
+    }
+    //Debido a que se usan observables, podemos usar una funcionalidad para reintentar peticiones is fallan con retry de rxjs
+    return this.http.get<Product[]>(this.apiUrl, { params }).pipe(retry(2));
   }
+
+  //Trae los productos con paginación (La función getAllProducts hace lo mismo)
+  // getProductsByPage(limit: number, offset: number) {
+  //   return this.http.get<Product[]>(`${this.apiUrl}`, {
+  //     params: { limit, offset }
+  //   })
+  // }
 
   //Trae la información de un producto
   getProduct(id: string) {
